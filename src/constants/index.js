a034ed72a398dae17e4698c6a6b224ec07ee5729
jaynwanze <105801651+jaynwanze@users.noreply.github.com>
2023-03-15 18:14:31 +0000

    third commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -131,7 +131,7 @@ import {
       date: "",
       points: [
         "Developing practical real world applications using Java swing/AWT frameworks and other related technologies.",
-        "Examples include responsive menu-driven GUI bank application using Swing/AWT whichwhich stores a set of bank account records associated to customers along with functionality to add, modify and delete records, carry out appropriate transactions on those records and produce bank statements both for individual bank accounts and for the collection of accounts.",
+        "Examples include responsive menu-driven GUI bank application using Swing/AWT which stores a set of bank account records associated to customers. Which includes functionality to add, modify and delete records, carry out appropriate transactions on those records and produce bank statements both for individual bank accounts and for the collection of accounts.",
       ],
     },
     {
